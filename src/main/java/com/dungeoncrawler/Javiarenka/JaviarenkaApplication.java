b5d19e86e41db607f2c70c66637e6d041110ab3f
epasper@gmail.com <epasper@gmail.com>
2020-08-27 22:38:33 +0200

    Reimported the SpringBoot

---
+++ src/main/java/com/dungeoncrawler/Javiarenka/JaviarenkaApplication.java
@@ -0,0 +1,13 @@
+package com.dungeoncrawler.Javiarenka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaviarenkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaviarenkaApplication.class, args);
+	}
+
+}
