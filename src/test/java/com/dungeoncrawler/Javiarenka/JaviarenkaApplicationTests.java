5c37c55a246bc993eb2130faf614795a990572a1
epasper@gmail.com <epasper@gmail.com>
2020-08-27 21:29:28 +0200

    Initial commit

---
+++ src/test/java/com/dungeoncrawler/Javiarenka/JaviarenkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dungeoncrawler.Javiarenka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaviarenkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
