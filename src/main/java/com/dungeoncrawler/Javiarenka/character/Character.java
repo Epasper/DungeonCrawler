555e007ec597b371767826751e6fbad784aa76f3
Karolina-BastaDrozen <karolina.basta90@gmail.com>
2020-09-02 21:33:21 +0200

    Adding an initial class

---
+++ src/main/java/com/dungeoncrawler/Javiarenka/character/Character.java
@@ -0,0 +1,23 @@
+package com.dungeoncrawler.Javiarenka.character;
+
+public class Character {
+
+    private String name;
+    private int hp;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+}
