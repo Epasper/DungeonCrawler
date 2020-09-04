1c7bc5834c2d1e1c04e703d85bb88949117f8cca
epasper@gmail.com <epasper@gmail.com>
2020-09-04 18:35:15 +0200

    Removed unnecessary imports

--- src/main/java/com/dungeoncrawler/Javiarenka/characterCreator/CharacterCreatorController.java
+++ src/main/java/com/dungeoncrawler/Javiarenka/characterCreator/CharacterCreatorController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
-import java.util.Set;
 
 
 @Controller
