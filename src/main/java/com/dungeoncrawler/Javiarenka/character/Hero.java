d18dec8b1202b56152532244ca0d95848781534f
epasper@gmail.com <epasper@gmail.com>
2020-09-04 23:23:47 +0200

    Changed the HTML styling and fixed the form POST method. Also Hero class scratch was created

---
+++ src/main/java/com/dungeoncrawler/Javiarenka/character/Hero.java
@@ -0,0 +1,58 @@
+package com.dungeoncrawler.Javiarenka.character;
+
+import com.dungeoncrawler.Javiarenka.equipment.Armor;
+import com.dungeoncrawler.Javiarenka.equipment.Weapon;
+
+public class Hero extends Character {
+    private String surname;
+    private HeroClass heroClass;
+    private Armor equippedArmor;
+    private Weapon equippedWeapon;
+
+    @Override
+    public String toString() {
+        return "Hero{" +
+                "name='" + super.getName() + '\'' +
+                "surname='" + surname + '\'' +
+                ", heroClass=" + heroClass +
+                ", equippedArmor=" + equippedArmor +
+                ", equippedWeapon=" + equippedWeapon +
+                '}';
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public HeroClass getHeroClass() {
+        return heroClass;
+    }
+
+    public void setHeroClass(HeroClass heroClass) {
+        this.heroClass = heroClass;
+    }
+
+    public Armor getEquippedArmor() {
+        return equippedArmor;
+    }
+
+    public void setEquippedArmor(Armor equippedArmor) {
+        this.equippedArmor = equippedArmor;
+    }
+
+    public Weapon getEquippedWeapon() {
+        return equippedWeapon;
+    }
+
+    public void setEquippedWeapon(Weapon equippedWeapon) {
+        this.equippedWeapon = equippedWeapon;
+    }
+
+    public void saveThisHero() {
+        //TODO: DUN-26: Zapisać postać w lokalnym pliku
+    }
+}
--- src/main/java/com/dungeoncrawler/Javiarenka/characterCreator/CharacterCreatorController.java
+++ src/main/java/com/dungeoncrawler/Javiarenka/characterCreator/CharacterCreatorController.java
@@ -1,9 +1,9 @@
 package com.dungeoncrawler.Javiarenka.characterCreator;
 
+import com.dungeoncrawler.Javiarenka.character.Hero;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 
 
 @Controller
@@ -14,11 +14,14 @@ public class CharacterCreatorController {
     @GetMapping("/characterCreator")
     public String characterCreatorGet(Model model) {
         model.addAttribute("heroClasses", service.getAvailableClassesStringified());
+        model.addAttribute("hero", new Hero());
         return "characterCreator";
     }
 
     @PostMapping("/characterCreator")
-    public String characterCreatorPost(Model model) {
-        return "character";
+    public String characterCreatorPost(@ModelAttribute Hero hero, Model model) {
+        System.out.println("POST TEST:" + hero.toString());
+        hero.saveThisHero();
+        return "characterCreator";
     }
 }
