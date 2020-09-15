aaac683393f7e9903396cc703704fba25d7ce208
epasper@gmail.com <epasper@gmail.com>
2020-09-15 18:42:56 +0200

    Changed the hero and monster selection process

--- src/main/java/com/dungeoncrawler/Javiarenka/Board/BoardController.java
+++ src/main/java/com/dungeoncrawler/Javiarenka/Board/BoardController.java
@@ -3,6 +3,8 @@ package com.dungeoncrawler.Javiarenka.Board;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class BoardController {
@@ -13,5 +15,19 @@ public class BoardController {
         model.addAttribute("heroes", service.getHeroes());
         model.addAttribute("monsters", service.getMonsters());
         return "fightBoard";
+    }
+
+    @GetMapping("/fightBoard/selectHero")
+    public RedirectView selectHero(Model model, @RequestParam(value = "heroName") String heroName) {
+        service.setSelectedHero(service.getCurrentHeroByName(heroName));
+        System.out.println("Selected Hero: " + service.getSelectedHero().getName());
+        return new RedirectView("");
+    }
+
+    @GetMapping("/fightBoard/selectMonster")
+    public RedirectView selectMonster(Model model, @RequestParam(value = "monsterName") String monsterName) {
+        service.setSelectedMonster(service.getCurrentMonsterByName(monsterName));
+        System.out.println("Selected Monster: " + service.getSelectedMonster().getName());
+        return new RedirectView("");
     }
 }
--- src/main/java/com/dungeoncrawler/Javiarenka/Board/BoardService.java
+++ src/main/java/com/dungeoncrawler/Javiarenka/Board/BoardService.java
@@ -9,12 +9,15 @@ import com.dungeoncrawler.Javiarenka.equipment.StartingWeapon;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
 
 public class BoardService {
     private List<Hero> heroes = new ArrayList<>();
     private List<Monster> monsters = new ArrayList<>();
+    private Monster selectedMonster;
+    private Hero selectedHero;
 
-    public BoardService(){
+    public BoardService() {
         heroes.add(new Hero("Joe", 70, "Jenkins", HeroClass.ARCHER, StartingArmor.CLOTH_ARMOR, StartingWeapon.SHORT_BOW, 20));
         heroes.add(new Hero("Mike", 120, "Trashold", HeroClass.KNIGHT, StartingArmor.RUSTED_CHAIN_ARMOR, StartingWeapon.SHORT_SWORD, 12));
         heroes.add(new Hero("Chase", 50, "Kingsman", HeroClass.WIZARD, StartingArmor.LEATHER_ARMOR, StartingWeapon.STAFF_OF_FIRE, 15));
@@ -37,5 +40,37 @@ public class BoardService {
 
     public void setMonsters(List<Monster> monsters) {
         this.monsters = monsters;
+    }
+
+    public Monster getSelectedMonster() {
+        return selectedMonster;
+    }
+
+    public void setSelectedMonster(Monster selectedMonster) {
+        this.selectedMonster = selectedMonster;
+    }
+
+    public Hero getSelectedHero() {
+        return selectedHero;
+    }
+
+    public void setSelectedHero(Hero selectedHero) {
+        this.selectedHero = selectedHero;
+    }
+
+    public Hero getCurrentHeroByName(String heroName) {
+        return heroes
+                .stream()
+                .filter(e -> e.getName().equalsIgnoreCase(heroName))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    public Monster getCurrentMonsterByName(String monsterName) {
+        return monsters
+                .stream()
+                .filter(e -> e.getName().equalsIgnoreCase(monsterName))
+                .findFirst()
+                .orElseThrow();
     }
 }
