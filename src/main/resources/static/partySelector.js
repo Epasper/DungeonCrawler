a9d473810480a0febf5d104f87facd34ffbecbd8
epasper@gmail.com <epasper@gmail.com>
2020-09-20 22:15:41 +0200

    JS functioning, css and styling to be done

--- src/main/resources/static/partySelector.js
+++ src/main/resources/static/partySelector.js
@@ -1,7 +1,25 @@
 let selectedCharacters = [];
 
-function selectCharacter(characterNameAndSurname){
-    selectedCharacters.push(characterNameAndSurname)
-    this.disabled=true;
-    console.log(characterNameAndSurname);
+function selectCharacter(characterNameAndSurname) {
+    let currentElement = document.getElementById(characterNameAndSurname);
+    console.log(currentElement.className);
+    currentElement.className === 'unselected' ? addCharacterToParty(characterNameAndSurname) : removeCharacterFromParty(characterNameAndSurname);
+}
+
+function addCharacterToParty(characterNameAndSurname) {
+    let currentElement = document.getElementById(characterNameAndSurname);
+    selectedCharacters.push(characterNameAndSurname);
+    currentElement.className = 'selected';
+    console.log(currentElement.className);
+    console.log('HeroAdded' + selectedCharacters.join('\n'));
+    return 'selected';
+}
+
+function removeCharacterFromParty(characterNameAndSurname) {
+    let currentElement = document.getElementById(characterNameAndSurname);
+    selectedCharacters = selectedCharacters.filter(e => e !== characterNameAndSurname);
+    currentElement.className = 'unselected';
+    console.log(currentElement.className);
+    console.log('HeroRemoved' + selectedCharacters.join('\n'));
+    return 'unselected';
 }
