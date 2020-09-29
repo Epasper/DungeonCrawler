14490f0565587d0e779e953ba9b696a5f2535668
epasper@gmail.com <epasper@gmail.com>
2020-09-29 21:18:09 +0200

    Start of view building

--- src/main/java/com/dungeoncrawler/Javiarenka/equipment/EquipmentController.java
+++ src/main/java/com/dungeoncrawler/Javiarenka/equipment/EquipmentController.java
@@ -1,4 +1,17 @@
 package com.dungeoncrawler.Javiarenka.equipment;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class EquipmentController {
+
+    EquipmentService service = new EquipmentService();
+
+    @GetMapping("/characterCreator")
+    public String backpackGet(Model model) {
+        model.addAttribute("hero", service.getCurrentlySelectedHero());
+        return "backpack";
+    }
 }
--- src/main/java/com/dungeoncrawler/Javiarenka/equipment/EquipmentService.java
+++ src/main/java/com/dungeoncrawler/Javiarenka/equipment/EquipmentService.java
@@ -1,8 +1,18 @@
 package com.dungeoncrawler.Javiarenka.equipment;
 
 import com.dungeoncrawler.Javiarenka.character.Hero;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EquipmentService {
 
     Hero currentlySelectedHero;
+
+    public Hero getCurrentlySelectedHero() {
+        return currentlySelectedHero;
+    }
+
+    public void setCurrentlySelectedHero(Hero currentlySelectedHero) {
+        this.currentlySelectedHero = currentlySelectedHero;
+    }
 }
