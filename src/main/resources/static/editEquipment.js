a282d79d8187235e02f1c95602d17280d385fee2
epasper@gmail.com <epasper@gmail.com>
2020-10-11 21:56:25 +0200

    Drag and drop dropping now changers JS objects accordingly

--- src/main/resources/static/editEquipment.js
+++ src/main/resources/static/editEquipment.js
@@ -1,5 +1,12 @@
 let currentlySelectedStartingSlot;
 let currentlySelectedEquipment;
+const emptySlot = {
+    name: "NO EQUIPMENT",
+    weight: 0,
+    price: 0,
+    classRestrictions: null,
+    imageLink: "../images/null"
+}
 
 function test() {
     console.log(currentHeroBackpack)
@@ -10,26 +17,37 @@ function allowDrop(event) {
 }
 
 function drag(event) {
-    let targetSlot
     const currentId = event.target.id;
-    if (currentId.includes('baggage')){
+    currentlySelectedStartingSlot = currentId;
+    if (currentId.includes('baggage')) {
         const baggageNumber = currentId.substring(8);
         console.log(baggageNumber);
         currentlySelectedEquipment = currentHeroBackpack.baggage[parseInt(baggageNumber)];
     } else {
         currentlySelectedEquipment = currentHeroBackpack[currentId];
     }
-    // console.log(event)
-    // console.log(currentHeroBackpack)
-    console.log(currentId)
-    console.log(currentlySelectedEquipment)
-    event.dataTransfer.setData("text", event.target.id);
+    // console.log(currentId)
+    // console.log(currentlySelectedEquipment)
 }
 
 function drop(event) {
+    const currentId = event.target.id;
+    if (currentId.includes('baggage')) {
+        const baggageNumber = currentId.substring(8);
+        currentHeroBackpack.baggage[parseInt(baggageNumber)] = currentlySelectedEquipment;
+    } else {
+        currentHeroBackpack[currentId] = currentlySelectedEquipment;
+    }
+    if (currentlySelectedStartingSlot.includes('baggage')) {
+        const baggageNumber = currentlySelectedStartingSlot.substring(8);
+        currentHeroBackpack.baggage[parseInt(baggageNumber)] = emptySlot;
+    } else {
+        currentHeroBackpack[currentlySelectedStartingSlot] = emptySlot;
+    }
+    currentlySelectedEquipment = null;
+    currentlySelectedStartingSlot = null;
     event.preventDefault();
-    console.log(event)
-    console.log(event.target.id)
-    var data = event.dataTransfer.getData("text");
-    event.target.appendChild(document.getElementById(data));
+    // console.log(event)
+    console.log(currentHeroBackpack)
+    // console.log(event.target.id)
 }
