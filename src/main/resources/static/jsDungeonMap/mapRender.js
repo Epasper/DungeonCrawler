e28315681f170ca370e5cc6ba8194aa79743ee58
hwnd-git <tomasz.evyl@gmail.com>
2020-10-12 21:10:53 +0200

    Party direction indicator is animated.

--- src/main/resources/static/jsDungeonMap/mapRender.js
+++ src/main/resources/static/jsDungeonMap/mapRender.js
@@ -1,5 +1,7 @@
 import { addParty, party } from './partyManager.js'
 
+let prevFacingDirection = 'none';
+
 export function draw() {
     //TODO: mapa jest przerysowywana co kliknięcie, ale to powoduje nieładne efekty wizualne na zaznaczonej drużynie
     // może by nie rysować party na nowo z każdym kliknięciem, jeśli nie zmiania się status samej drużyny
@@ -16,8 +18,7 @@ async function drawParty() {
 
     const [xCoord, yCoord, direction] = [backendParty.occupiedTile.x, backendParty.occupiedTile.y, backendParty.direction];
     console.log(`drawing party: x:${xCoord}/ y:${yCoord} facing: ${direction}`);
-
-    debugger;
+    //debugger;
 
     let partyTile = document.getElementById(`party`);
     let partyImg = document.getElementById('party-img');
@@ -25,6 +26,8 @@ async function drawParty() {
         console.log('Creating Party div')
         partyTile = document.createElement(`div`)
         partyTile.id = `party`;
+        // const grid = document.getElementById('grid');
+        // grid.appendChild(partyTile);
 
         partyImg = document.createElement('img');
         partyImg.id = 'party-img';
@@ -39,31 +42,60 @@ async function drawParty() {
     partyTile.style.gridRow = `${yCoord + 1}`;
     partyTile.style.gridColumn = `${xCoord + 1}`;
 
-    const grid = document.getElementById('grid');
-    grid.appendChild(partyTile);
+    // const grid = document.getElementById('grid');
+    // grid.appendChild(partyTile);
 
     if(partyImg){
+        let standardAnimationDuration = 0.1;
+        const longerAnimationMultiplier = 2.5;
+        
         switch (direction) {
             case 'UP':
                 partyImg.style.opacity = ('0.4');
-                partyImg.style.transform = ('rotate(-90deg)')
+                if(prevFacingDirection == 'RIGHT') {
+                    partyImg.style.animationName = `${prevFacingDirection.toLowerCase()}, right-to-up`
+                    break;
+                } else if(prevFacingDirection == 'DOWN') {
+                    standardAnimationDuration *= longerAnimationMultiplier;
+                }
+                partyImg.style.animationName = `${prevFacingDirection.toLowerCase()}, up`
                 break;
             case 'LEFT':
                 partyImg.style.opacity = ('0.4');
-                partyImg.style.transform = ('rotate(180deg)')
+                if(prevFacingDirection == 'RIGHT') {
+                    standardAnimationDuration *= longerAnimationMultiplier;
+                }
+                partyImg.style.animationName = `${prevFacingDirection.toLowerCase()}, left`
                 break;
             case 'RIGHT':
                 partyImg.style.opacity = ('0.4');
-                partyImg.style.transform = ('rotate(0deg)')
+                if(prevFacingDirection == 'UP') {
+                    partyImg.style.animationName = `${prevFacingDirection.toLowerCase()}, up-to-right`
+                    break;
+                } else if (prevFacingDirection == 'LEFT') {
+                    standardAnimationDuration *= longerAnimationMultiplier;
+                }
+                partyImg.style.animationName = `${prevFacingDirection.toLowerCase()}, right`
                 break;
             case 'DOWN':
                 partyImg.style.opacity = ('0.4');
-                partyImg.style.transform = ('rotate(90deg)')
+                if(prevFacingDirection == 'UP') {
+                    standardAnimationDuration *= longerAnimationMultiplier;
+                }
+                partyImg.style.animationName = `${prevFacingDirection.toLowerCase()}, down`
                 break;
             default:
                 partyImg.style.opacity = ('0');
+                partyImg.style.transition = 'all 0.5s'
+        }
+        partyImg.style.animationDuration = `0s, ${standardAnimationDuration}s`
+        if(direction){
+            prevFacingDirection = direction;
         }
     }
+
+    const grid = document.getElementById('grid');
+    grid.appendChild(partyTile);
 
     if (!party) addParty(partyTile);
 }
