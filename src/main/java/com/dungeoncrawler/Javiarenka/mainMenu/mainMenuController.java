5911cd3a73b9b6e4ec375e5c993f314acb13df81
ewa-ilnicka <ewa.h.ilnicka@gmail.com>
2020-10-15 20:35:25 +0200

    Added controllers to menu

--- src/main/java/com/dungeoncrawler/Javiarenka/mainMenu/mainMenuController.java
+++ src/main/java/com/dungeoncrawler/Javiarenka/mainMenu/mainMenuController.java
@@ -1,2 +1,29 @@
-package com.dungeoncrawler.Javiarenka.mainMenu;public class mainMenuController {
+package com.dungeoncrawler.Javiarenka.mainMenu;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class mainMenuController {
+
+    @GetMapping("/mainMenu")
+    public String getMainMenu() {
+        return "mainMenu";
+    }
+
+    @GetMapping("/newGame")
+    public String getNewGame() {
+        return "newGame";
+    }
+
+    @GetMapping("/loadGame")
+    public String getLoadGame() {
+        return "loadGame";
+    }
+
+    @GetMapping("/aboutProject")
+    public String getAboutProject() {
+        return "aboutProject";
+    }
+
 }
