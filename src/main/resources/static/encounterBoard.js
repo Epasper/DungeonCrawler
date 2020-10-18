ac33b33759144dca745ff3fca6fd05dc1202da51
epasper@gmail.com <epasper@gmail.com>
2020-10-18 21:28:06 +0200

    Hover effects

--- src/main/resources/static/encounterBoard.js
+++ src/main/resources/static/encounterBoard.js
@@ -4,6 +4,7 @@ function initialize() {
         const image = '../images/' + element.heroClass + '.png';
         console.log(id + "|||" + image);
         let heroDiv = document.getElementById(id);
+        heroDiv.style.zIndex = "3";
         heroDiv.src = image;
     });
     console.log('initialized!');
