3e67f5c2c2921ff5997357b50859997ec832a866
epasper@gmail.com <epasper@gmail.com>
2020-10-20 21:21:13 +0200

    Encounter Board finished, but no monsters are being spawned yet

--- src/main/resources/static/encounterBoard.js
+++ src/main/resources/static/encounterBoard.js
@@ -1,7 +1,6 @@
 let globalSelectedHero
 
 function initialize() {
-    document.onload = setOnClicksOnAllTiles();
     document.onload = heroes.forEach(element => {
         const id = 'hero:' + element.encounterXPosition + '---' + element.encounterYPosition;
         const image = '../images/' + element.heroClass + '.png';
@@ -9,59 +8,53 @@ function initialize() {
         let heroDiv = document.getElementById(id);
         heroDiv.style.zIndex = "3";
         heroDiv.src = image;
-        heroDiv.onclick = () => getMoveableTiles(element);
+        heroDiv.onclick = () => getMovableTiles(element);
     });
-    console.log('initialized!');
 }
 
-function getMoveableTiles(curentlyClickedHero) {
+function getMovableTiles(curentlyClickedHero) {
     globalSelectedHero = curentlyClickedHero;
     clearTheBoardOfWalkEffects();
     const heroSpeed = curentlyClickedHero.speed;
     const xPos = curentlyClickedHero.encounterXPosition;
     const yPos = curentlyClickedHero.encounterYPosition;
     const isSelected = curentlyClickedHero.isSelected;
-    for (i = xPos - heroSpeed; i < xPos + heroSpeed; i++) {
-        if (i < 1) continue;
-        for (j = yPos - heroSpeed; j < yPos + heroSpeed; j++) {
-            if (j < 1) continue;
+    for (i = xPos - heroSpeed; i < xPos + heroSpeed + 1; i++) {
+        if (i < 1 || i > width) continue;
+        for (j = yPos - heroSpeed; j < yPos + heroSpeed + 1; j++) {
+            if (j < 1 || j > height) continue;
             let currentEffectDiv = document.getElementById('container' + i + '---' + j);
-            let currentDiv = document.getElementById(i + '---' + j);
             currentEffectDiv.classList.add('walkable-effects');
-            currentDiv.classList.add('walkable-tile');
             tiles[i - 1][j - 1].withinReach = true;
         }
     }
     curentlyClickedHero.isSelected = !isSelected;
 }
 
-function setOnClicksOnAllTiles() {
-    for (i = 1; i <= width; i++) {
-        for (j = 1; j <= height; j++) {
-            let currentDiv = document.getElementById('container' + i + '---' + j);
-            currentDiv.onclick = () => moveTheHero(i, j);
-        }
-    }
-}
-
 function clearTheBoardOfWalkEffects() {
     for (i = 1; i <= width; i++) {
         for (j = 1; j <= height; j++) {
             let currentEffectDiv = document.getElementById('container' + i + '---' + j);
-            let currentDiv = document.getElementById(i + '---' + j);
             currentEffectDiv.classList.remove('walkable-effects');
-            currentDiv.classList.remove('walkable-tile');
         }
     }
 }
 
-function moveTheHero(x, y) {
-    console.log('StartWalking')
-    globalSelectedHero.encounterXPosition = x;
-    globalSelectedHero.encounterYPosition = y;
+function moveTheHero(currentId) {
+    const xPosition = parseInt(currentId.slice(0, currentId.indexOf('---')));
+    const yPosition = parseInt(currentId.slice(currentId.indexOf('---') + 3));
+    const oldXPos = globalSelectedHero.encounterXPosition;
+    const oldYPos = globalSelectedHero.encounterYPosition;
+    let oldDiv = document.getElementById('hero:' + oldXPos + '---' + oldYPos);
+    oldDiv.style.zIndex = "-1";
+    delete oldDiv.src;
+    globalSelectedHero.encounterXPosition = xPosition;
+    globalSelectedHero.encounterYPosition = yPosition;
     const image = '../images/' + globalSelectedHero.heroClass + '.png';
-    const id = 'hero:' + globalSelectedHero.encounterXPosition + '---' + globalSelectedHero.encounterYPosition;
-    let currentDiv = document.getElementById(id);
+    const heroId = 'hero:' + currentId;
+    let currentDiv = document.getElementById(heroId);
     currentDiv.src = image;
-    console.log('Walking! ' + x + ' -- ' + y)
+    currentDiv.style.zIndex = "3";
+    clearTheBoardOfWalkEffects()
+    initialize();
 }
