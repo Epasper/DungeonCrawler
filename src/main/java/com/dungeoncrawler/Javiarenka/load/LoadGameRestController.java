5b3ba616f5f6f523526f565e41984884fd2d99d5
ewa-ilnicka <ewa.h.ilnicka@gmail.com>
2020-11-02 21:15:03 +0100

    Added RestController do LoadGame

--- src/main/java/com/dungeoncrawler/Javiarenka/load/LoadGameRestController.java
+++ src/main/java/com/dungeoncrawler/Javiarenka/load/LoadGameRestController.java
@@ -1,4 +1,20 @@
 package com.dungeoncrawler.Javiarenka.load;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+
+@RestController
 public class LoadGameRestController {
+    LoadGameService loadGameService = new LoadGameService();
+
+    @GetMapping("/getAllSaves")
+    public RedirectView getAllSaves(@RequestParam String numberToLoad) {
+        loadGameService.load(numberToLoad);
+        loadGameService.load();
+        return new RedirectView("encounterBoard");
+    }
+
 }
