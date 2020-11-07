49c95001ca50afc5ac485f2b03b441b83e0b508b
ewa-ilnicka <ewa.h.ilnicka@gmail.com>
2020-11-07 21:15:43 +0100

    Added logic in Shop Controller

--- src/main/java/com/dungeoncrawler/Javiarenka/shop/ShopController.java
+++ src/main/java/com/dungeoncrawler/Javiarenka/shop/ShopController.java
@@ -1,8 +1,19 @@
 package com.dungeoncrawler.Javiarenka.shop;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ShopController {
     ShopService service = new ShopService();
+
+    @GetMapping("/shop")
+    public String getShop(Model model) {
+        System.out.println("lubie placki");
+        model.addAttribute("merchant", service.getMerchant());
+        model.addAttribute("hero", service.getCurrentlyTradingHero());
+        model.addAttribute("party", service.getParty());
+        return "shop";
+    }
 }
